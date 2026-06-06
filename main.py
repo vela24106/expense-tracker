c112779e3cbd61a536a4bfa09501f37fbaf51a83
vela24106 <velanp2006@gmail.com>
2026-06-06 11:20:13 +0530

    save and loed  expense feature

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-print("\tEXPENSES TRAKER")
 expense = []
 
 def add_expense():
@@ -8,8 +7,8 @@ def add_expense():
 def view_expense(expense):
     count =1
     for i in expense:
-         print ("expense",count,":",i)
-
+         print("expense",count,":",i)
+         
          count +=1
 
 def total_expense(expense):
@@ -27,14 +26,32 @@ def del_expense():
 
     else:
        print ("NO EXPENSES")
-    
 
+
+def save_expense():
+    
+     with open("expense.txt","w")as f:
+         for i in expense:
+            f.write(str(i) + "\n")
+     print("saved")
+
+def load_expense():
+    expense.clear()
+    with open("expense.txt","r")as f:
+        for line in f:
+            value = int(line.strip())
+            expense.append(value)
+
+    print("loaded !!!")
+           
 while True:
     print("1. Add Expense")
     print("2. View Expense")
     print("3. Total Expense")
     print("4. Delete Expense")
-    print("5. exit")
+    print("5. Save Expense")
+    print("6. Load_Expense")
+    print("7. Exit")
     choice = int(input("enter the choice :"))
     match choice:
         case 1 :
@@ -45,8 +62,7 @@ while True:
         case 2:
            print("view_expense")
            print("------------")
-           view_expense(expense)
-        
+           view_expense(expense)         
         case 3:
              print("Total Expense =", total_expense(expense))
         case 4:
@@ -55,7 +71,17 @@ while True:
             del_expense()
             
         case 5:
-             break
+             save_expense()
+
+        case 6:
+            load_expense()
+        case 7:
+            break
 
         case _:
             print("-----invalid choice-----")
+
+
+
+
+
